7a3484baa212501be3a9573fcf2bd75f944e0faf
donna41827 <donna41827@gmail.com>
2021-01-15 19:44:44 +0800

    20210115_Ver1

---
+++ LaughtersHundredImage/Form1.Designer.cs
@@ -0,0 +1,249 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace LaughtersHundredImage
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.pbImgLeft = new System.Windows.Forms.PictureBox();
+            this.txtFileUrl = new System.Windows.Forms.TextBox();
+            this.btnUpload = new System.Windows.Forms.Button();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.btnSizeBig = new System.Windows.Forms.Button();
+            this.btnSizeSmaller = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnChoose100Img = new System.Windows.Forms.Button();
+            this.openFileDialog2 = new System.Windows.Forms.OpenFileDialog();
+            this.BtnExportImgA2 = new System.Windows.Forms.Button();
+            this.lblMsg = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pbImgLeft)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // pbImgLeft
+            // 
+            this.pbImgLeft.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pbImgLeft.Location = new System.Drawing.Point(29, 68);
+            this.pbImgLeft.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.pbImgLeft.Name = "pbImgLeft";
+            this.pbImgLeft.Size = new System.Drawing.Size(300, 300);
+            this.pbImgLeft.TabIndex = 0;
+            this.pbImgLeft.TabStop = false;
+            this.pbImgLeft.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pbImgLeft_MouseDown);
+            this.pbImgLeft.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pbImgLeft_MouseMove);
+            this.pbImgLeft.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pbImgLeft_MouseUp);
+            this.pbImgLeft.MouseWheel += new System.Windows.Forms.MouseEventHandler(this.pbImgLeft_MouseWheel);
+            // 
+            // txtFileUrl
+            // 
+            this.txtFileUrl.Location = new System.Drawing.Point(20, 20);
+            this.txtFileUrl.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.txtFileUrl.Name = "txtFileUrl";
+            this.txtFileUrl.Size = new System.Drawing.Size(173, 31);
+            this.txtFileUrl.TabIndex = 1;
+            // 
+            // btnUpload
+            // 
+            this.btnUpload.Location = new System.Drawing.Point(277, 16);
+            this.btnUpload.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.btnUpload.Name = "btnUpload";
+            this.btnUpload.Size = new System.Drawing.Size(60, 39);
+            this.btnUpload.TabIndex = 2;
+            this.btnUpload.Text = "上傳";
+            this.btnUpload.UseVisualStyleBackColor = true;
+            this.btnUpload.Click += new System.EventHandler(this.btnUpload_Click);
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(203, 16);
+            this.btnBrowse.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(64, 39);
+            this.btnBrowse.TabIndex = 3;
+            this.btnBrowse.Text = "瀏覽";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // btnSizeBig
+            // 
+            this.btnSizeBig.Location = new System.Drawing.Point(260, 378);
+            this.btnSizeBig.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.btnSizeBig.Name = "btnSizeBig";
+            this.btnSizeBig.Size = new System.Drawing.Size(69, 39);
+            this.btnSizeBig.TabIndex = 4;
+            this.btnSizeBig.Text = "放大";
+            this.btnSizeBig.UseVisualStyleBackColor = true;
+            this.btnSizeBig.Click += new System.EventHandler(this.btnSizeBig_Click);
+            // 
+            // btnSizeSmaller
+            // 
+            this.btnSizeSmaller.Location = new System.Drawing.Point(29, 378);
+            this.btnSizeSmaller.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.btnSizeSmaller.Name = "btnSizeSmaller";
+            this.btnSizeSmaller.Size = new System.Drawing.Size(70, 39);
+            this.btnSizeSmaller.TabIndex = 5;
+            this.btnSizeSmaller.Text = "縮小";
+            this.btnSizeSmaller.UseVisualStyleBackColor = true;
+            this.btnSizeSmaller.Click += new System.EventHandler(this.btnSizeSmaller_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 472);
+            this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 20);
+            this.label1.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(25, 492);
+            this.label2.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 20);
+            this.label2.TabIndex = 7;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(29, 427);
+            this.btnSave.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(66, 39);
+            this.btnSave.TabIndex = 8;
+            this.btnSave.Text = "確認";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(396, 67);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(600, 425);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnChoose100Img
+            // 
+            this.btnChoose100Img.Location = new System.Drawing.Point(396, 15);
+            this.btnChoose100Img.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnChoose100Img.Name = "btnChoose100Img";
+            this.btnChoose100Img.Size = new System.Drawing.Size(70, 39);
+            this.btnChoose100Img.TabIndex = 10;
+            this.btnChoose100Img.Text = "選取";
+            this.btnChoose100Img.UseVisualStyleBackColor = true;
+            this.btnChoose100Img.Click += new System.EventHandler(this.btnChoose100Img_Click);
+            // 
+            // openFileDialog2
+            // 
+            this.openFileDialog2.FileName = "openFileDialog2";
+            // 
+            // BtnExportImgA2
+            // 
+            this.BtnExportImgA2.Location = new System.Drawing.Point(474, 15);
+            this.BtnExportImgA2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.BtnExportImgA2.Name = "BtnExportImgA2";
+            this.BtnExportImgA2.Size = new System.Drawing.Size(131, 39);
+            this.BtnExportImgA2.TabIndex = 11;
+            this.BtnExportImgA2.Text = "產出A2圖檔";
+            this.BtnExportImgA2.UseVisualStyleBackColor = true;
+            this.BtnExportImgA2.Click += new System.EventHandler(this.BtnExportImgA2_Click);
+            // 
+            // lblMsg
+            // 
+            this.lblMsg.AutoSize = true;
+            this.lblMsg.Font = new System.Drawing.Font("新細明體", 12F);
+            this.lblMsg.ForeColor = System.Drawing.Color.Red;
+            this.lblMsg.Location = new System.Drawing.Point(614, 25);
+            this.lblMsg.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblMsg.Name = "lblMsg";
+            this.lblMsg.Size = new System.Drawing.Size(0, 20);
+            this.lblMsg.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1021, 521);
+            this.Controls.Add(this.lblMsg);
+            this.Controls.Add(this.BtnExportImgA2);
+            this.Controls.Add(this.btnChoose100Img);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSizeSmaller);
+            this.Controls.Add(this.btnSizeBig);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.btnUpload);
+            this.Controls.Add(this.txtFileUrl);
+            this.Controls.Add(this.pbImgLeft);
+            this.Font = new System.Drawing.Font("新細明體", 12F);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "Form1";
+            this.Text = "百笑圖產生器";
+            ((System.ComponentModel.ISupportInitialize)(this.pbImgLeft)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.PictureBox pbImgLeft;
+        private System.Windows.Forms.TextBox txtFileUrl;
+        private System.Windows.Forms.Button btnUpload;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.Button btnSizeBig;
+        private System.Windows.Forms.Button btnSizeSmaller;
+        private Label label1;
+        private Label label2;
+        private Button btnSave;
+        private PictureBox pictureBox1;
+        private Button btnChoose100Img;
+        private OpenFileDialog openFileDialog2;
+        private Button BtnExportImgA2;
+        private Label lblMsg;
+    }
+}
+
---
+++ LaughtersHundredImage/Form1.cs
@@ -0,0 +1,318 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Encoder = System.Drawing.Imaging.Encoder;
+
+namespace LaughtersHundredImage
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string sEditImgPath = "";
+        int MouseDownX = 0;
+        int MouseDownY = 0;
+        bool MouseDrapFlag = false;
+        double ReSizeRate = 1;
+        int ImgX = 0;
+        int ImgY = 0;
+        int A2Width = 6480;
+        int A2Height = 4860;
+        int A2Square = 540;
+        Bitmap HundredBmap;
+        string palettesPath = "c:\\LaughtersHundredImg";
+        string palettesPath2 = "c:\\LaughtersHundredImg\\Title";
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            var fileContent = string.Empty;
+            var filePath = string.Empty;
+            openFileDialog1.InitialDirectory = "c:\\";
+            openFileDialog1.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            openFileDialog1.FilterIndex = 2;
+            openFileDialog1.RestoreDirectory = true;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                filePath = openFileDialog1.FileName;
+                var fileStream = openFileDialog1.OpenFile();
+                using (StreamReader reader = new StreamReader(fileStream))
+                {
+                    fileContent = reader.ReadToEnd();
+                }
+                txtFileUrl.Text = filePath;
+            }
+        }
+        private void btnUpload_Click(object sender, EventArgs e)
+        {
+            sEditImgPath = txtFileUrl.Text;
+            pbImgLeft.Load(txtFileUrl.Text);
+            ReSizeRate = 1;
+            ImgX = 0;
+            ImgY = 0;
+            pbImgLeft.Focus();
+            //label1.Text = "ReSizeRate:" + ReSizeRate + ",ImgX:" + ImgX + ",ImgY:" + ImgY;
+        }
+        private void ScaleImage(int MouseX, int MouseY, double Rate)
+        {
+            try
+            {
+                Bitmap pBmp = new Bitmap(sEditImgPath);
+                ReSizeRate = ReSizeRate * Rate;
+                ImgX = MouseX - Convert.ToInt32(Math.Round((MouseX - ImgX) * Rate, 0));
+                ImgY = MouseY - Convert.ToInt32(Math.Round((MouseY - ImgY) * Rate, 0));
+                Bitmap tmpBmp = new Bitmap(300, 300);
+                Graphics g = Graphics.FromImage(tmpBmp);
+                g.DrawImage(pBmp,
+                            new Rectangle(ImgX, ImgY, Convert.ToInt32(Math.Round(pBmp.Width * ReSizeRate, 0)), Convert.ToInt32(Math.Round(pBmp.Height * ReSizeRate, 0))),
+                            new Rectangle(0, 0, pBmp.Width, pBmp.Height),
+                            GraphicsUnit.Pixel
+                            );
+                pbImgLeft.Image = tmpBmp;
+                g.Dispose();
+                //label1.Text = "ReSizeRate:" + ReSizeRate + ",ImgX:" + ImgX + ",ImgY:" + ImgY + ",Rate:" + Rate + ",pBmp.Width:" + pBmp.Width + ",pBmp.Height:" + pBmp.Height + ",CalW:" + Convert.ToInt32(Math.Round(pBmp.Width * ReSizeRate, 0)) + ",CALH:" + Convert.ToInt32(Math.Round(pBmp.Height * ReSizeRate, 0));
+
+            }
+            catch (Exception ex) { }
+        }
+        private Bitmap MoveImage(int MouseX, int MouseY)
+        {
+            try
+            {
+                Bitmap pBmp = new Bitmap(sEditImgPath);
+                Bitmap tmpBmp = new Bitmap(300, 300);
+                Graphics g = Graphics.FromImage(tmpBmp);
+                g.DrawImage(pBmp,
+                            new Rectangle(ImgX + (MouseX - MouseDownX), ImgY + (MouseY - MouseDownY), Convert.ToInt32(Math.Round(pBmp.Width * ReSizeRate, 0)), Convert.ToInt32(Math.Round(pBmp.Height * ReSizeRate, 0))),
+                            new Rectangle(0, 0, pBmp.Width, pBmp.Height),
+                            GraphicsUnit.Pixel
+                            );
+                pbImgLeft.Image = tmpBmp;
+                g.Dispose();
+                //label1.Text = "ReSizeRate:" + ReSizeRate + ",ImgX:" + ImgX + ",ImgY:" + ImgY + ",pBmp.Width:" + pBmp.Width + ",pBmp.Height:" + pBmp.Height + ",CalW:" + Convert.ToInt32(Math.Round(pBmp.Width * ReSizeRate, 0)) + ",CALH:" + Convert.ToInt32(Math.Round(pBmp.Height * ReSizeRate, 0));
+                return tmpBmp;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+
+        private void btnSizeBig_Click(object sender, EventArgs e)
+        {
+            ScaleImage(150, 150, 1.1);
+        }
+        private void btnSizeSmaller_Click(object sender, EventArgs e)
+        {
+            ScaleImage(150, 150, 0.9);
+        }
+        private void pbImgLeft_MouseWheel(object sender, MouseEventArgs e)
+        {
+            if (e.Delta > 0)
+            {
+                ScaleImage(e.X, e.Y, 1.1);
+            }
+            else if (e.Delta < 0)
+            {
+                ScaleImage(e.X, e.Y, 0.9);
+            }
+        }
+        private void pbImgLeft_MouseDown(object sender, MouseEventArgs e)
+        {
+            pbImgLeft.Focus();
+            MouseDownX = e.X;
+            MouseDownY = e.Y;
+            MouseDrapFlag = true;
+        }
+        private void pbImgLeft_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (MouseDrapFlag)
+            {
+                MoveImage(e.X, e.Y);
+            }
+            //label2.Text = "x:" + e.X + ",y:" + e.Y;
+        }
+        private void pbImgLeft_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (MouseDrapFlag)
+            {
+                MoveImage(e.X, e.Y);
+                ImgX = ImgX + (e.X - MouseDownX);
+                ImgY = ImgY + (e.Y - MouseDownY);
+                //label2.Text = "UP_x:" + ImgX + ",y:" + ImgY;
+            }
+            MouseDrapFlag = false;
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Bitmap pBmp = new Bitmap(sEditImgPath);
+                Bitmap tmpBmp = new Bitmap(A2Square, A2Square);
+                Graphics tmpG = Graphics.FromImage(tmpBmp);
+                tmpG.DrawImage(pBmp,
+                               new Rectangle(Convert.ToInt32(Math.Round(Convert.ToDouble(ImgX) * A2Square / 300.0, 0)),
+                                             Convert.ToInt32(Math.Round(Convert.ToDouble(ImgY) * A2Square / 300.0, 0)),
+                                             Convert.ToInt32(Math.Round(Convert.ToDouble(pBmp.Width) * ReSizeRate * A2Square / 300.0, 0)),
+                                             Convert.ToInt32(Math.Round(Convert.ToDouble(pBmp.Height) * ReSizeRate * A2Square / 300.0, 0))),
+                               new Rectangle(0,
+                                             0,
+                                             pBmp.Width, pBmp.Height),
+                               GraphicsUnit.Pixel);
+                Bitmap GBitMap = new Bitmap(A2Square, A2Square, tmpG);
+                //label1.Text = "ReSizeRate:" + ReSizeRate + ",X:" + Convert.ToInt32(Math.Round(Convert.ToDouble(ImgX) * A2Square / 300.0, 0)) + ",Y:" + Convert.ToInt32(Math.Round(Convert.ToDouble(ImgY) * A2Square / 300.0, 0));
+                SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+
+                string curDir;
+                curDir = GetImgStoragePath(palettesPath);//Directory.GetCurrentDirectory().Replace("\\bin\\Debug", "\\Image");
+                saveFileDialog1.InitialDirectory = curDir;
+
+                saveFileDialog1.Filter = "JPG File|*.jpg";
+                saveFileDialog1.Title = "儲存影像檔";
+                saveFileDialog1.FilterIndex = 3;
+                if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    saveJpeg(saveFileDialog1.FileName, tmpBmp, 85);
+                }
+                tmpG.Dispose();
+            }
+            catch (Exception ex) { }
+        }
+        private void saveJpeg(string path, Bitmap img, long quality)
+        {
+            EncoderParameter qualityParam = new EncoderParameter(Encoder.Quality, quality);
+            ImageCodecInfo jpegCodec = this.getEncoderInfo("image/png");
+            if (jpegCodec == null) return;
+            EncoderParameters encoderParams = new EncoderParameters(1);
+            encoderParams.Param[0] = qualityParam;
+            img.Save(path, jpegCodec, encoderParams);
+        }
+        private ImageCodecInfo getEncoderInfo(string mimeType)
+        {
+            ImageCodecInfo[] codecs = ImageCodecInfo.GetImageEncoders();
+            for (int i = 0; i < codecs.Length; i++)
+                if (codecs[i].MimeType == mimeType)
+                    return codecs[i];
+            return null;
+        }
+        private void btnChoose100Img_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var fileContent = string.Empty;
+                var filePath = string.Empty;
+                openFileDialog2.InitialDirectory = GetImgStoragePath(palettesPath2);//Directory.GetCurrentDirectory().Replace("\\bin\\Debug", "\\Image");
+                openFileDialog2.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+                openFileDialog2.FilterIndex = 2;
+                openFileDialog2.RestoreDirectory = true;
+                openFileDialog2.Multiselect = true;
+
+                //基準座標(留白邊)
+                //double BaseX = 17.79;
+                //double BaseY = 23.99;
+                //double BigBaseX = 208;
+                //double BigBaseY = 280.5;
+                //int BaseWH = 47;
+                int BigBaseWH = A2Square;
+                lblMsg.Text = "";
+                if (openFileDialog2.ShowDialog() == DialogResult.OK)
+                {
+                    if (openFileDialog2.FileNames.Length > 100) lblMsg.Text = "選取" + openFileDialog2.FileNames.Length + "張圖片，將以前100張顯示!";
+                    else lblMsg.Text = "選取" + openFileDialog2.FileNames.Length + "張圖片";
+                    HundredBmap = new Bitmap(A2Width, A2Height, PixelFormat.Format32bppArgb);
+
+                    HundredBmap.MakeTransparent();
+                    Graphics g = Graphics.FromImage(HundredBmap);
+                    g.Clear(Color.Transparent);
+                    int idx = 0;
+                    for (int i = 0; i < openFileDialog2.FileNames.Length; i++)
+                    {
+                        if (i == 40) idx = 4;
+                        else if (i == 48) idx = 8;  
+                        Bitmap pBmp = new Bitmap(openFileDialog2.FileNames[i]);
+                        g.DrawImage(pBmp,
+                                    new Rectangle(Convert.ToInt32(Math.Round((BigBaseWH * ((i + idx) % 12.0)), 0)), Convert.ToInt32(Math.Round((BigBaseWH * Math.Floor((i + idx) / 12.0)), 0)), A2Square, A2Square),
+                                    new Rectangle(0, 0, pBmp.Width, pBmp.Height),
+                                    GraphicsUnit.Pixel
+                                    );
+                        if (i == 100) break;
+                    }
+                    ChkExistsTitleImg();
+                    Bitmap pBmpTitle = new Bitmap(palettesPath2 + "\\Title.jpg");
+                    g.DrawImage(pBmpTitle,
+                                    new Rectangle(Convert.ToInt32(Math.Round((BigBaseWH * 4.0), 0)), Convert.ToInt32(Math.Round((BigBaseWH * 3.0), 0)), A2Square*4, A2Square*2),
+                                    new Rectangle(0, 0, pBmpTitle.Width, pBmpTitle.Height),
+                                    GraphicsUnit.Pixel
+                                    );
+                    pictureBox1.Image = HundredBmap;
+
+                }
+            }
+            catch (Exception ex)
+            {
+                //    lblMsg.Text = ex.ToString();
+            }
+        }
+
+        private void BtnExportImgA2_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            string curDir;
+            curDir = "c:\\";//GetImgStoragePath(palettesPath);//Directory.GetCurrentDirectory().Replace("\\bin\\Debug", "");
+            saveFileDialog1.InitialDirectory = curDir;
+
+            saveFileDialog1.Filter = "PNG File|*.png";
+            saveFileDialog1.Title = "儲存影像檔";
+            saveFileDialog1.FilterIndex = 3;
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                saveJpeg(saveFileDialog1.FileName, HundredBmap, 100);
+            }
+        }
+        private string GetImgStoragePath(string sPalettesPath)
+        {
+            try
+            {
+                if (!Directory.Exists(sPalettesPath))
+                {
+                    Directory.CreateDirectory(sPalettesPath);
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+            return palettesPath;
+        }
+        private void ChkExistsTitleImg()
+        {
+            try
+            {
+                if (!Directory.Exists(palettesPath2))
+                {
+                    Directory.CreateDirectory(palettesPath2);
+                }
+                if (!File.Exists(palettesPath2 + "\\Title.jpg"))
+                {
+                    //Bitmap bTitle = new Bitmap(@"../../Resources/Title.jpg");
+                    Bitmap bTitle = new Bitmap(Properties.Resources.Title);
+                    saveJpeg(palettesPath2 + "\\Title.jpg", bTitle, 100);
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+    }
+}
